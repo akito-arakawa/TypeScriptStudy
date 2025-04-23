e27a5db84490416e45a74e2c4b3ade8d3d2654e7
akito <kd1373996@st.kobedenshi.ac.jp>
2025-04-23 21:45:12 +0900

    基本的な文法

---
+++ dist/sample.js
@@ -0,0 +1,10 @@
+import { createInterface } from "node:readline";
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question("文字列を入力してください:", (line) => {
+    const result = Number(line) + 1000;
+    console.log(result);
+    rl.close();
+});
